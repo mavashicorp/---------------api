43046abbce6a5387189e252aa944a9a235aa4e98
MAK <mavashicorp>
2024-03-13 01:05:24 +0200

    внес поля для вывода данных в блок main

--- js/script.js
+++ js/script.js
@@ -127,32 +127,35 @@
 
 // ///////////////////////////////
 // //Получаем прогноз в массив data
-function getWeather() {
-  fetch(
-    "http://api.openweathermap.org/data/2.5/weather?id=703494&lang=ru&appid=34747752906cc3846e5748a60c5d8956"
-  )
-    .then(function (resp) {
-      return resp.json();
-    })
-    .then(function (data) {
-      //добавляем название города
-      document.querySelector(".weather__city").textContent = data.name;
-      console.log(data.name);
-      //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
-      document.querySelector(".weather__forecast").innerHTML =
-        Math.round(data.main.temp - 273) + "&deg;";
-      //Добавляем описание погоды
-      document.querySelector(".weather__desc").textContent =
-        data.weather[0]["description"];
-      //Добавляем иконку погоды
-      document.querySelector(
-        ".weather__icon"
-      ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
-    })
-    .catch(function () {
-      //Обрабатываем ошибки
-    });
-}
+// function getWeather() {
+//   fetch(
+//     "http://api.openweathermap.org/data/2.5/weather?id=703494&lang=ru&appid=34747752906cc3846e5748a60c5d8956"
+//   )
+//     .then(function (resp) {
+//       return resp.json();
+//     })
+//     .then(function (data) {
+//       //добавляем название города
+//       //document.querySelector(".weather__city").textContent = data.name;
+//       console.log(data.name);
+
+
+//       // Пример кода для вывода данных в зону полигона
+//       // //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
+//       // document.querySelector(".weather__forecast").innerHTML =
+//       //   Math.round(data.main.temp - 273) + "&deg;";
+//       // //Добавляем описание погоды
+//       // document.querySelector(".weather__desc").textContent =
+//       //   data.weather[0]["description"];
+//       // //Добавляем иконку погоды
+//       // document.querySelector(
+//       //   ".weather__icon"
+//       // ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+//     })
+//     .catch(function () {
+//       //Обрабатываем ошибки
+//     });
+// }
 
 //////////////////////////////
 
